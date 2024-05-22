c9acf1e0096c40c3042d20662527dd7d46013179
Ayelen Ibañez <ibanezayelen307@gmail.com>
2024-05-21 21:18:34 -0300

    Creacion de la clase postre

---
+++ Trabajo Final Laboratorio III/src/Clases/Postre.java
@@ -0,0 +1,28 @@
+package Clases;
+
+public class Postre extends Comida {
+    private String nombreDelPostre;
+    private String descripcion;  //descripcion de los ingredientes que contiene el postre.
+
+    public Postre(String nombreComida, double precioComida,String nombreDelPostre,String descripcion) {
+        super(nombreComida, precioComida);
+        this.nombreDelPostre = nombreDelPostre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombreDelPostre() {
+        return nombreDelPostre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Postre{" +
+                "nombreDelPostre='" + nombreDelPostre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+}
