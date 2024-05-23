f0e7713be26e24b63477589840cd0c4d5293ac4e
Anahi Brocardo <anahibrocardo213@gmail.com>
2024-05-22 21:10:18 -0300

    Creacion de la clase Tarjeta

--- Trabajo Final Laboratorio III/src/Clases/PagoEfectivo.java
+++ Trabajo Final Laboratorio III/src/Clases/PagoEfectivo.java
@@ -7,5 +7,5 @@ public class PagoEfectivo extends Pago{
         super(monto);
         this.descuento = descuento;
     }
-    
+
 }
--- Trabajo Final Laboratorio III/src/Clases/PagoTarjeta.java
+++ Trabajo Final Laboratorio III/src/Clases/PagoTarjeta.java
@@ -1,8 +1,20 @@
 package Clases;
 
 public class PagoTarjeta extends Pago{
-    
-    public PagoTarjeta(double monto) {
+    private Persona cliente;
+    private int numeroTarjeta;
+    private int cantidadCuotas;
+    private String tipoDeTarjeta; //Visa,Mastercard
+    private String tipoDeCuenta; //credito o debito
+
+    public PagoTarjeta(double monto, Persona cliente, int numeroTarjeta, int cantidadCuotas, String tipoDeTarjeta, String tipoDeCuenta) {
         super(monto);
+        this.cliente = cliente;
+        this.numeroTarjeta = numeroTarjeta;
+        this.cantidadCuotas = 0; //inicializamos la cantidad de cuotas en 0, ya que solo se va a poder ingresar la cantidad de cuotas si la tarjeta es credito
+        this.tipoDeTarjeta = tipoDeTarjeta;
+        this.tipoDeCuenta = tipoDeCuenta;
     }
+
+
 }
