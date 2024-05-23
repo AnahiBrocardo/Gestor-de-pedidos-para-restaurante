c22b14b0d3ea3e9352c5211e9cd5278ae391f01f
Anahi Brocardo <anahibrocardo213@gmail.com>
2024-05-22 21:49:14 -0300

    Creacion de clase Cerveza

---
+++ Trabajo Final Laboratorio III/src/Clases/Cerveza.java
@@ -0,0 +1,48 @@
+package Clases;
+
+public class Cerveza extends Bebida{
+    private String marca; // artesanal, quilmes, corona
+    private double nivelAlcohol; // Nivel de alcohol en porcentaje
+    private int amargura; //indice de amargura (por ejemplo, IBU)
+
+
+    public Cerveza(float capacidad, String marca, double nivelAlcohol, int amargura) {
+        super(capacidad);
+        this.marca = marca;
+        this.nivelAlcohol = nivelAlcohol;
+        this.amargura = amargura;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getNivelAlcohol() {
+        return nivelAlcohol;
+    }
+
+    public void setNivelAlcohol(double nivelAlcohol) {
+        this.nivelAlcohol = nivelAlcohol;
+    }
+
+    public int getAmargura() {
+        return amargura;
+    }
+
+    public void setAmargura(int amargura) {
+        this.amargura = amargura;
+    }
+
+    @Override
+    public String toString() {
+        return "Cerveza{" +
+                "marca='" + marca + '\'' +
+                ", nivelAlcohol=" + nivelAlcohol +
+                ", amargura=" + amargura +
+                '}';
+    }
+}
