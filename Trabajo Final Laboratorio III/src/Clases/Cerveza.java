a1c9ad434911cf50bb54395ac6dba50213c99ae4
Anahi Brocardo <anahibrocardo213@gmail.com>
2024-05-24 11:27:02 -0300

    Cambios en la clase Cerveza

--- Trabajo Final Laboratorio III/src/Clases/Cerveza.java
+++ Trabajo Final Laboratorio III/src/Clases/Cerveza.java
@@ -1,47 +1,31 @@
 package Clases;
 
 public class Cerveza extends Bebida{
-    private String marca; // artesanal, quilmes, corona
     private double nivelAlcohol; // Nivel de alcohol en porcentaje
     private int amargura; //indice de amargura (por ejemplo, IBU)
 
 
-    public Cerveza(String nombreElementoMenu, double precioElementoMenu, float capacidad, String marca, double nivelAlcohol, int amargura) {
-        super(nombreElementoMenu, precioElementoMenu, capacidad);
-        this.marca = marca;
+    public Cerveza(String nombreElementoMenu, double precioElementoMenu, float capacidad, String marca, String sabor, double nivelAlcohol, int amargura) {
+        super(nombreElementoMenu, precioElementoMenu, capacidad, marca, sabor);
         this.nivelAlcohol = nivelAlcohol;
         this.amargura = amargura;
     }
 
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
 
     public double getNivelAlcohol() {
         return nivelAlcohol;
     }
 
-    public void setNivelAlcohol(double nivelAlcohol) {
-        this.nivelAlcohol = nivelAlcohol;
-    }
 
     public int getAmargura() {
         return amargura;
     }
 
-    public void setAmargura(int amargura) {
-        this.amargura = amargura;
-    }
 
     @Override
     public String toString() {
-        return "Cerveza{" +
-                "marca='" + marca + '\'' +
-                ", nivelAlcohol=" + nivelAlcohol +
+        return super.toString()+" Cerveza{" +
+                "nivelAlcohol=" + nivelAlcohol +
                 ", amargura=" + amargura +
                 '}';
     }
