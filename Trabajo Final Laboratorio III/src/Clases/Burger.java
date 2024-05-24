b7f16a894223092249935b259c650e513e010bcb
Ayelen Ibañez <ibanezayelen307@gmail.com>
2024-05-24 18:37:45 -0300

    Creacion de claseBurger (bien escrita)

---
+++ Trabajo Final Laboratorio III/src/Clases/Burger.java
@@ -0,0 +1,38 @@
+package Clases;
+
+public class Burger extends ElementoMenu {
+        private String descripcion;
+        private String tipoHamburguesa; //puede ser Mexicana, americana, argenta o liviana
+
+        public Burger (String nombreElementoMenu, double precioElementoMenu, String descripcion, String tipoHamburguesa) {
+            super(nombreElementoMenu, precioElementoMenu);
+            this.descripcion = descripcion;
+            this.tipoHamburguesa = tipoHamburguesa;
+        }
+
+        public String getDescripcion() {
+            return descripcion;
+        }
+
+        public void setDescripcion(String descripcion) {
+            this.descripcion = descripcion;
+        }
+
+        public String getTipoHamburguesa() {
+            return tipoHamburguesa;
+        }
+
+        public void setTipoHamburguesa(String tipoHamburguesa) {
+            this.tipoHamburguesa = tipoHamburguesa;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString()+ " Burger{" +
+                    "descripcion='" + descripcion + '\'' +
+                    ", tipoHamburguesa='" + tipoHamburguesa + '\'' +
+                    '}';
+        }
+    }
+
+
