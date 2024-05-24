864be08a6e08c956be93b2d56c5679147a538a22
Anahi Brocardo <anahibrocardo213@gmail.com>
2024-05-24 11:25:13 -0300

    Cambios en clase Bebida
    Se agregaron nuevos atributos, se modifico el constructor y el metodo toSting(), ademas de agregar los getters.

--- Trabajo Final Laboratorio III/src/Clases/Bebida.java
+++ Trabajo Final Laboratorio III/src/Clases/Bebida.java
@@ -2,16 +2,34 @@ package Clases;
 
 public  abstract class Bebida extends ElementoMenu {
     private float capacidad; //500ml, 1000ml, 1500mlt o 2250ml;
+    private String marca; // antares, corona, quilmes
+    private String sabor;// rubia, roja
 
-    public Bebida(String nombreElementoMenu, double precioElementoMenu, float capacidad) {
+    public Bebida(String nombreElementoMenu, double precioElementoMenu, float capacidad, String marca, String sabor) {
         super(nombreElementoMenu, precioElementoMenu);
         this.capacidad = capacidad;
+        this.marca = marca;
+        this.sabor = sabor;
+    }
+
+    public float getCapacidad() {
+        return capacidad;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getSabor() {
+        return sabor;
     }
 
     @Override
     public String toString() {
-        return super.toString()+ " Bebida{" +
+        return super.toString()+ "Bebida{" +
                 "capacidad=" + capacidad +
+                ", marca='" + marca + '\'' +
+                ", sabor='" + sabor + '\'' +
                 '}';
     }
 }
