619e4092d5ec507d4a62c45d454a3ba791252fbd
Julieta Alijas <julietaalijas94@gmail.com>
2024-05-25 13:32:35 -0300

    Funcion Agregar
    Sumo funcion agregar al mapa

--- Trabajo Final Laboratorio III/src/FuncionesMapa/GenericidadMapa.java
+++ Trabajo Final Laboratorio III/src/FuncionesMapa/GenericidadMapa.java
@@ -12,7 +12,21 @@ public class GenericidadMapa  <E> implements IFunciones {
     public GenericidadMapa() {
         nuevomapa = new HashMap<>();
     }
-    
+
+    @Override
+    public void agregar(Object o, String key) {
+        ArrayList<Object> datosArreglo;
+        if(nuevomapa.containsKey(key)){
+            datosArreglo= nuevomapa.get(key);
+            datosArreglo.add(o);
+        } else {
+            datosArreglo= new ArrayList<>();
+            datosArreglo.add(o);
+            nuevomapa.put(key,datosArreglo);
+        }
+
+    }
+
 
 
 
