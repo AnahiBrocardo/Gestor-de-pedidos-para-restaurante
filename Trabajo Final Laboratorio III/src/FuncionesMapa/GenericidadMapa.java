c1ce58265e56000e722731d53ce2a89a542d6be3
Julieta Alijas <julietaalijas94@gmail.com>
2024-05-25 13:31:38 -0300

    Craar Clase Mapa

--- Trabajo Final Laboratorio III/src/FuncionesMapa/GenericidadMapa.java
+++ Trabajo Final Laboratorio III/src/FuncionesMapa/GenericidadMapa.java
@@ -1,5 +1,19 @@
 package FuncionesMapa;
 
+
+import Interfaces.IFunciones;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class GenericidadMapa  <E> implements IFunciones {
+    private HashMap <String, ArrayList<Object>> nuevomapa;
+
+    public GenericidadMapa() {
+        nuevomapa = new HashMap<>();
+    }
     
+
+
+
 }
