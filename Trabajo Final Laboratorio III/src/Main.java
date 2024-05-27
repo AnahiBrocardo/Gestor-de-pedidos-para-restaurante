8abc716b4b11ae6b4fd0242198fc2af390f55ffb
Ayelen Ibañez <ibanezayelen307@gmail.com>
2024-05-27 20:42:34 -0300

    Revert "Importe las clases al main"
    This reverts commit 05caf5dc960d2267a803eec69c0437a2231a2970.

--- Trabajo Final Laboratorio III/src/Main.java
+++ Trabajo Final Laboratorio III/src/Main.java
@@ -1,19 +1,10 @@
 
-import Clases.*;
-
-import java.util.Date;
+import Clases.Cerveza;
+import Clases.ElementoMenu;
 
 public class Main {
     public static void main(String[] args) {
-        Pedido nuevoP= new Pedido(1254, new Date());
-        ElementoMenu nuevaC= new Burger("Burger",1500,"jkfef","Mexicana");
-        ElementoMenu nuevaC2= new Burger("Burger",2000,"jkfef","Americana");
-        ElementoMenu nuevaC3= new Postre("Postre",1449,"jkfef","Brownie con helado");
 
-        nuevoP.getConjuntoDeElementos().agregar(nuevaC, "Mexicana");
-        nuevoP.getConjuntoDeElementos().agregar(nuevaC2, "Americana");
-        nuevoP.getConjuntoDeElementos().agregar(nuevaC3, "Brownie con helado");
-        System.out.println("El total del pedidod es: "+nuevoP.calcularTotaldelPedido());
 
     }
 }
