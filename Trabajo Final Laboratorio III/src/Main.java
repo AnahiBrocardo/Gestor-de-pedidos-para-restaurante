05caf5dc960d2267a803eec69c0437a2231a2970
Anahi Brocardo <anahibrocardo213@gmail.com>
2024-05-27 20:17:37 -0300

    Importe las clases al main

--- Trabajo Final Laboratorio III/src/Main.java
+++ Trabajo Final Laboratorio III/src/Main.java
@@ -1,10 +1,19 @@
 
-import Clases.Cerveza;
-import Clases.ElementoMenu;
+import Clases.*;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        Pedido nuevoP= new Pedido(1254, new Date());
+        ElementoMenu nuevaC= new Burger("Burger",1500,"jkfef","Mexicana");
+        ElementoMenu nuevaC2= new Burger("Burger",2000,"jkfef","Americana");
+        ElementoMenu nuevaC3= new Postre("Postre",1449,"jkfef","Brownie con helado");
 
+        nuevoP.getConjuntoDeElementos().agregar(nuevaC, "Mexicana");
+        nuevoP.getConjuntoDeElementos().agregar(nuevaC2, "Americana");
+        nuevoP.getConjuntoDeElementos().agregar(nuevaC3, "Brownie con helado");
+        System.out.println("El total del pedidod es: "+nuevoP.calcularTotaldelPedido());
 
     }
 }
