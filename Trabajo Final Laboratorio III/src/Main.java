4ad2b101659c42eeb1de79701057548f4aff6a9c
Anahi Brocardo <anahibrocardo213@gmail.com>
2024-05-29 17:18:43 -0300

    prueba main

--- Trabajo Final Laboratorio III/src/Main.java
+++ Trabajo Final Laboratorio III/src/Main.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-
+/*
         ElementoMenu n1 = new Burger("Burger", 2340, "chees and bacom", "Mexicana");
         ElementoMenu n2 = new Postre("Postre", 2370, "brownie con helado", "brofje");
         Pedido c2 = new Pedido(123, new Date());
@@ -14,6 +14,9 @@ public class Main {
         c2.agregarApedido(n2, "brownie con helado");
 
         System.out.println(c2.calcularTotaldelPedido());
-        System.out.println(c2.toString());
+        System.out.println(c2.toString());*/
+
+        Menu nuevoMenu= new Menu();
+        System.out.println(nuevoMenu.listarTodoMenu());
     }
 }
