27ede5ee70b8998ea5af281e1543bfed31754b71
Anahi Brocardo <anahibrocardo213@gmail.com>
2024-05-29 13:22:43 -0300

    Creacion del metodo lecturaArchivo() de la clase ControladoraJson
    Se creo el metodo lecturaArchivo, donde se lee el archivo y se obtiene el texto plano del json, y luego con la clave "postre" se accede a los objetos del arreglo y de cada  objeto con la clave se obtienen los datos, y se agrega el elemento menu al mapa.

--- Trabajo Final Laboratorio III/src/Clases/ControladoraJson.java
+++ Trabajo Final Laboratorio III/src/Clases/ControladoraJson.java
@@ -12,7 +12,45 @@ public class ControladoraJson {
     /*Este metodo leer el archivo archivoMenu.bin  y lo codifica a la clase Menu
      * @return el mapa de elementosMenu (comida) del archivo*/
 
-    
+    public HashMap<String, ArrayList<ElementoMenu>> lecturaArchivo()
+    {
+
+        String jsonRespuesta= JsonUtiles.leer("archivoMenu.bin");
+        HashMap<String, ArrayList<ElementoMenu>> mapaMenu= new HashMap<>();
+
+        try{
+            JSONObject jsonObject= new JSONObject(jsonRespuesta);
+
+            JSONArray arregloPostres= jsonObject.getJSONArray("postre");
+            for (int i=0; i<arregloPostres.length(); i++){
+                ArrayList<ElementoMenu> datos;
+                JSONObject jsonObjetArregloPostre= arregloPostres.getJSONObject(i);
+
+                String nombreElementoMenu=jsonObjetArregloPostre.getString("nombreElementoMenu");
+                Double precioElementoMenu= jsonObjetArregloPostre.getDouble("precioElementoMenu");
+                String nombrePostre= jsonObjetArregloPostre.getString("nombreDelPostre");
+                String descripcion=  jsonObjetArregloPostre.getString("descripcion");
+                ElementoMenu nuevoE= new Postre(nombreElementoMenu, precioElementoMenu,nombrePostre,descripcion);
+
+                ArrayList<ElementoMenu> datosArreglo;
+                if(mapaMenu.containsKey("postre")){
+                    datosArreglo= mapaMenu.get("postre");
+                    datosArreglo.add(nuevoE);
+                }else{
+                    datosArreglo= new ArrayList<>();
+                    datosArreglo.add(nuevoE);
+                    mapaMenu.put("postre", datosArreglo);
+                }
+
+
+            }
+        }
+        catch (JSONException exception){
+            exception.printStackTrace();
+        }
+
+        return mapaMenu;
+    }
 
 
 
