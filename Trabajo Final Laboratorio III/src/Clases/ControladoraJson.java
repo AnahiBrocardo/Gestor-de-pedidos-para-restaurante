3e6559f0dc8d3aab72122fbe03d7e24d88fb0d29
Anahi Brocardo <anahibrocardo213@gmail.com>
2024-05-29 13:19:31 -0300

    Creacion clase controladora

---
+++ Trabajo Final Laboratorio III/src/Clases/ControladoraJson.java
@@ -0,0 +1,22 @@
+package Clases;
+
+import com.sun.jdi.ArrayReference;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ControladoraJson {
+    /*Este metodo leer el archivo archivoMenu.bin  y lo codifica a la clase Menu
+     * @return el mapa de elementosMenu (comida) del archivo*/
+
+    
+
+
+
+}
+
+
+
