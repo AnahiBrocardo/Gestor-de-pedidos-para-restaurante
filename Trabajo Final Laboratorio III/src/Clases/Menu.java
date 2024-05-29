e985d8c6b7503e4b18fdb3aafd006180317742b0
Anahi Brocardo <anahibrocardo213@gmail.com>
2024-05-29 15:44:38 -0300

    Creacion de clase Menu

---
+++ Trabajo Final Laboratorio III/src/Clases/Menu.java
@@ -0,0 +1,15 @@
+package Clases;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Menu {
+ private HashMap<String, ArrayList<ElementoMenu>> mapaDelMenu;
+
+    public Menu(HashMap<String, ArrayList<ElementoMenu>> mapaDelMenu) {
+        this.mapaDelMenu = mapaDelMenu;
+    }
+
+    
+}
