f17b2ab2503667b7dd524faf4670c5c9a086f189
Anahi Brocardo <anahibrocardo213@gmail.com>
2024-05-29 20:02:43 -0300

    Prueba en menu

--- Trabajo Final Laboratorio III/src/Main.java
+++ Trabajo Final Laboratorio III/src/Main.java
@@ -1,9 +1,24 @@
 
-import Clases.Burger;
-import Clases.Cerveza;
-import Clases.ElementoMenu;
+import Clases.*;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+/* PRUEBA MENU
+        ElementoMenu n1 = new Burger("Burger", 2340, "chees and bacom", "Mexicana");
+        ElementoMenu n2 = new Postre("Postre", 2370, "brownie con helado", "brofje");
+        Pedido c2 = new Pedido(123, new Date());
+
+        c2.agregarApedido(n1, "Mexicana");
+        c2.agregarApedido(n2, "brownie con helado");
+
+        System.out.println(c2.calcularTotaldelPedido());
+        System.out.println(c2.toString());
+
+        Menu nuevoMenu= new Menu();
+        System.out.println(nuevoMenu.listarTodoMenu());*/
     }
-}
+
+    }
+
