7de3ee57e8247c1a9cd997ad2962d67ab9271fab
Anahi Brocardo <anahibrocardo213@gmail.com>
2024-05-29 19:47:24 -0300

    Borrar prueba en main

--- Trabajo Final Laboratorio III/src/Main.java
+++ Trabajo Final Laboratorio III/src/Main.java
@@ -5,18 +5,6 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-/*
-        ElementoMenu n1 = new Burger("Burger", 2340, "chees and bacom", "Mexicana");
-        ElementoMenu n2 = new Postre("Postre", 2370, "brownie con helado", "brofje");
-        Pedido c2 = new Pedido(123, new Date());
 
-        c2.agregarApedido(n1, "Mexicana");
-        c2.agregarApedido(n2, "brownie con helado");
-
-        System.out.println(c2.calcularTotaldelPedido());
-        System.out.println(c2.toString());*/
-
-        Menu nuevoMenu= new Menu();
-        System.out.println(nuevoMenu.listarTodoMenu());
     }
 }
