a752e596ec8b0d2504ce9307d1811cf01d6b9835
Ayelen Ibañez <ibanezayelen307@gmail.com>
2024-05-30 16:29:15 -0300

    Metodo grabarArchivo en controladoraArchivoCaja

--- Trabajo Final Laboratorio III/src/Archivos/ControladoraArchivoCaja.java
+++ Trabajo Final Laboratorio III/src/Archivos/ControladoraArchivoCaja.java
@@ -1,5 +1,36 @@
 package Archivos;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 public class ControladoraArchivoCaja {
-    
+    public static void grabarArchivoCaja (ArrayList<Caja> arrayListDeCajas){
+        FileOutputStream fileOutputStream= null;
+        ObjectOutputStream objectOutputStream= null;
+
+        try{
+            fileOutputStream= new FileOutputStream("cajas.dat");
+            objectOutputStream= new ObjectOutputStream(fileOutputStream);
+
+            for(int i=0;i<arrayListDeCajas.size(); i++){ //recorre el arreglo, obtiene el objeto estadistica por el indice
+                objectOutputStream.writeObject(arrayListDeCajas.get(i));
+            }
+
+        }catch (IOException exception){
+            System.out.println(exception.getMessage());
+        }
+        finally {
+            try {
+                if (fileOutputStream != null)
+                    fileOutputStream.close();
+
+                if (objectOutputStream != null)
+                    objectOutputStream.close();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
 }
