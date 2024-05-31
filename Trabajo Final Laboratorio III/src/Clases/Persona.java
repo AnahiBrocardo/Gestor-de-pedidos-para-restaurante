f5f623b477ac77f73b6037f527c74614be57591d
AnahiBrocardo <anahibrocardo213@gmail.com>
2024-05-31 13:45:46 -0300

    Creacion del metodo validarTelefonoPersona

--- Trabajo Final Laboratorio III/src/Clases/Persona.java
+++ Trabajo Final Laboratorio III/src/Clases/Persona.java
@@ -72,5 +72,13 @@ public class Persona {
         }
     }
 
+    public static void validarTelefonoPersona (String telefonoPersonal) throws InvalidCardNumberException {
+
+        if(telefonoPersonal.isEmpty() || telefonoPersonal.length()<10){
+            throw new InvalidDniExcepcion("Telefono invalido menor a 10 digitos");
+        }
+    }
+
+
 
 }
