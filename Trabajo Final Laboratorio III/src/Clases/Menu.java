e365aacf905b344f996586ce80156912e9e0a0fd
AnahiBrocardo <anahibrocardo213@gmail.com>
2024-06-05 15:06:39 -0300

    Agregamos la funcion listarMenuPorDigitos

--- Trabajo Final Laboratorio III/src/Clases/Menu.java
+++ Trabajo Final Laboratorio III/src/Clases/Menu.java
@@ -14,25 +14,38 @@ public class Menu{
 
 
 
-    public String listarTodoMenu(){
+    public String listarTodoMenu(){ //esta funcion lista todo el menu
         String rta="";
         Iterator<Map.Entry<String, ArrayList<ElementoMenu>>> iteratormap= mapaDelMenu.entrySet().iterator();
         while (iteratormap.hasNext()){
             Map.Entry<String, ArrayList<ElementoMenu>> entry = iteratormap.next();
             String key = entry.getKey();
-            rta += listarMenu(key);
+            rta+= listarMenu(key);
         }
 
         return rta;
     }
 
-    public String listarMenu(String key)
+    public String listarMenu(String key) //esta funcion lista solo el menu de determinado elemento del menu
     {
         String rta="";
         ArrayList<ElementoMenu> nuevoArreglo= mapaDelMenu.get(key);
         for(int i=0; i< nuevoArreglo.size(); i++)
         {
             rta += nuevoArreglo.get(i).toString() + "\n";
+        }
+        return rta;
+    }
+
+    public String listarMenuPorDigitos(String key) //esta funcion lista solo el menu de determinado elemento del menu
+    {
+        String rta="";
+        int digito=1;
+        ArrayList<ElementoMenu> nuevoArreglo= mapaDelMenu.get(key);
+        for(int i=0; i< nuevoArreglo.size(); i++)
+        {
+            rta += "\n.....Opcion "+digito+ nuevoArreglo.get(i).toString() + "\n";
+            digito++;
         }
         return rta;
     }
